2b096ff6c2b12749843a67b0d8e358f58ec83781
Jim Laney <jlaney@uw.edu>
2022-11-09 08:55:58 -0800

    update to v2

--- uw_space/tests/test_dao.py
+++ uw_space/tests/test_dao.py
@@ -1,5 +1,6 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from unittest import TestCase
 import mock
@@ -16,5 +17,4 @@ class TestSpaceDao(TestCase):
         dao = SPACE_DAO()
         self.assertEqual(dao.service_name(), "space")
         self.assertTrue(
-            dao.service_mock_paths()[0].endswith(
-                "uw-restclients-space/uw_space/resources"))
+            dao.service_mock_paths()[0].endswith("/uw_space/resources"))
